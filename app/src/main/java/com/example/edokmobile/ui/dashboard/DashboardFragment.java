29bc9c3362fa1a953144ec5ab5ce282e3ebd9491
Anastas <A.ru>
2024-11-08 00:01:24 +0500

    creating a table for recipes

--- app/src/main/java/com/example/edokmobile/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/edokmobile/ui/dashboard/DashboardFragment.java
@@ -4,17 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.edokmobile.R;
 import com.example.edokmobile.databinding.FragmentDashboardBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private ListView listView;
+
+    // creating  a String type array (fruitNames)
+    // which contains names of different fruits' images
+    String fruitNames[] = {"Banana", "Grape", "Guava", "Mango", "Orange", "Watermelon"};
+
+    // creating an Integer type array (fruitImageIds) which
+    // contains IDs of different fruits' images
+    int fruitImageIds[] = {R.drawable.ic_dashboard_black_24dp,
+            R.drawable.ic_dashboard_black_24dp,
+            R.drawable.ic_dashboard_black_24dp,
+            R.drawable.ic_dashboard_black_24dp,
+            R.drawable.ic_dashboard_black_24dp,
+            R.drawable.ic_dashboard_black_24dp};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +42,20 @@ public class DashboardFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
+
+        listView = binding.listView;
+        ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+        for (int i = 0; i < fruitNames.length; i++) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("fruitName", fruitNames[i]);
+            map.put("fruitImage", fruitImageIds[i]);
+            list.add(map);
+        }
+        String[] from = {"fruitName", "fruitImage"};
+        int to[] = {R.id.textView, R.id.imageRecipe};
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getContext().getApplicationContext(), list, R.layout.list_row_items, from, to);
+        listView.setAdapter(simpleAdapter);
+
         return root;
     }
 
