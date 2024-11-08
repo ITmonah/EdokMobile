1bccf10d1ede0e618a40496e2e8ee1d73b59a702
Anastas <A.ru>
2024-11-08 09:47:16 +0500

    change image

--- app/src/main/java/com/example/edokmobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/edokmobile/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textView;
+        final TextView textView = binding.textView3;
         return root;
     }
 
