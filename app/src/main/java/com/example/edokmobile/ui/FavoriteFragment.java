26704c75c211be7df75ab8c34681317f28107fd1
ITmonah <vasin.sasha26@gmail.com>
2024-12-18 21:08:17 +0500

    adding fav fragment

---
+++ app/src/main/java/com/example/edokmobile/ui/FavoriteFragment.java
@@ -0,0 +1,38 @@
+package com.example.edokmobile.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.example.edokmobile.MyApplication;
+import com.example.edokmobile.R;
+import com.example.edokmobile.databinding.FragmentFavoriteBinding;
+import com.example.edokmobile.databinding.FragmentProfileBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+public class FavoriteFragment extends Fragment {
+
+    private FragmentFavoriteBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
