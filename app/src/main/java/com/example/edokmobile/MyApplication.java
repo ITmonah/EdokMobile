31d06a70fcc5e0f6d09d43133b59a881f33a3f93
Anastas <A.ru>
2025-03-16 14:27:55 +0500

    Firebase push

--- app/src/main/java/com/example/edokmobile/MyApplication.java
+++ app/src/main/java/com/example/edokmobile/MyApplication.java
@@ -3,6 +3,9 @@ package com.example.edokmobile;
 import android.app.Application;
 import android.content.Context;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 import java.io.FileNotFoundException;
@@ -18,7 +21,7 @@ public class MyApplication extends Application {
 
     private GoogleSignInAccount account;
     protected OkHttpClient client = new OkHttpClient();
-    private String url = "http://10.0.2.2:8001/";
+    private String url = "https://zkjxvmvq-8000.euw.devtunnels.ms/";
     private String accessToken;
     private ArrayList<HashMap<String, Object>> user_info;
 //    @Override
@@ -67,5 +70,16 @@ public class MyApplication extends Application {
 
     public ArrayList<HashMap<String, Object>> getUserInfo() {
         return user_info;
+    }
+
+    //функции для токена телефона
+    private MutableLiveData<String> token = new MutableLiveData<>();
+
+    public void setToken(String myToken){
+        token.postValue(myToken);
+    }
+
+    public LiveData<String> getToken(){
+        return token;
     }
 }
